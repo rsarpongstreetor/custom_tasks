f1697d866dc9b9385194a887b1415a1d91d3d979
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-02-11 19:19:59 +0800

    Update task_1.py

--- environment/customenv/task_1.py
+++ environment/customenv/task_1.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, MISSING
 
 @dataclass
 class TaskConfig:
-    max_steps: int =100
+    max_steps: int =10
     supports_continuous_actions:bool=True
     supports_discrete_actions: bool=True
 
